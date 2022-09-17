e63ff09ec0f59ed6e2487a26de9e0191bf612b85
Adriana Giraldo <1531nana@gmail.com>
2022-09-17 11:25:56 -0500

    ignoré los archivos de test de fetch

--- __mocks__/mockFetch.js
+++ __mocks__/mockFetch.js
@@ -1,11 +1,19 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
-const fetchMock = () => {
-  return fetch(
-    "https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions"
-  ).then((data) => data);
-};
+// // const fetchMock = () => {
+// //   return fetch(
+// //     "https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions"
+// //   )
+// //   // .then((data) => data);
+// // };
 
-module.exports = {
-  fetchMock,
-};
+// const createLink = () => {
+//   const response = fetch(
+//     "https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions"
+//   )
+//   return response
+// };
+
+// module.exports = {
+//   createLink,
+// };
